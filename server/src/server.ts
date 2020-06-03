ceda6acce21a54cd148e113de28a9858371d14db
Alan Martins <alanhorcrux@gmail.com>
2020-06-02 21:25:36 -0300

    Criado Migrations para Banco SQLLite usando o Knex

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,32 +1,11 @@
 import express, { json } from 'express';
+import routes from './routes';
 
 const app = express();
 
-const users = [
-    'Alan',
-    'Clayton',
-    'Jose'
-];
+app.use(express.json()); // Entende Json
 
-app.get('/users',(request, response)=>{
-    response.json(users);
-});
+app.use(routes);
 
-app.get('/users/:id',(request, response)=>{
-    const id = Number(request.params.id);
-    
-    const user = users[id];
-    
-    response.json(user);
-});
-
-app.post('/users',(request, response) => {
-    const user = {
-        name: 'Alan',
-        email: 'Alan@alan.com'
-    };
-
-    return response.json(user);
-})
 
 app.listen(3333);
