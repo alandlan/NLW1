c7ff17bde49bf87dbdfc7166ec3c9236f43c3653
Alan Martins <alanhorcrux@gmail.com>
2020-06-03 22:46:20 -0300

    Criado pagina Home e Cadastro

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
+
+const Createpoint = () => {
+    return(
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Logo"/>
+                <Link to="/">
+                    <FiArrowLeft />
+                    Voltar para Home
+                </Link>
+            </header>
+            <form>
+                <h1>Cadastro do <br /> ponto de Coleta</h1>
+
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+                
+                    <div className="field">
+                        <label htmlFor="name">Nome</label>
+                        <input 
+                            type="text" 
+                            name="name"
+                            id="name"   
+                        />
+                    </div>
+
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="email">E-mail</label>
+                            <input 
+                            type="email" 
+                            name="email"
+                            id="email"   
+                        />
+                        </div>
+                        <div className="field">
+                            <label htmlFor="whatsapp">Whatsapp</label>
+                            <input 
+                            type="text" 
+                            name="whatsapp"
+                            id="whatsapp"   
+                        />
+                        </div>
+                    </div>
+                
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selectione o endereço no mapa</span>
+                    </legend>
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione</option>
+                            </select>
+                        </div>
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Ítens de Coleta</h2>
+                        <span>Selectione um ou mais ítens abaixo</span>
+                    </legend>
+                    <ul className="items-grid">
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste" />
+                            <span>Lampada</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste" />
+                            <span>Lampada</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste" />
+                            <span>Lampada</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste" />
+                            <span>Lampada</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste" />
+                            <span>Lampada</span>
+                        </li>
+                        <li>
+                            <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste" />
+                            <span>Lampada</span>
+                        </li>
+                    </ul>
+                </fieldset>
+                <button type="submit">
+                    Cadastrar ponto de Coleta
+                </button>
+            </form>
+
+        </div>
+    )
+};
+
+export default Createpoint;
